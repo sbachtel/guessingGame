e6df1b44f3f0702366ba751cd5825f34a9c85bdb
Steve Bachtel <sbachtel@gmail.com>
2016-04-19 18:52:27 -0700

    changed my questions and worked on the counter

--- app.js
+++ app.js
@@ -1,37 +1,57 @@
-
-var pElOne = document.getElementById('answerOne');
-var pElTwo = document.getElementById('answerTwo');
-var pElThree = document.getElementById('answerThree');
-
 var els = [
-  pElOne,
-  pElTwo,
-  pElThree,
+  document.getElementById('answerOne'),
+  document.getElementById('answerTwo'),
+  document.getElementById('answerThree'),
+  document.getElementById('answerFour'),
+  document.getElementById('answerFive'),
 ];
 
 var questions = [
-  "What is your name?",
+  "Color is a turtle?",
   "Would you like to play a game?",
-  "What number am I thinking of?",
+  "I am thinking of a number between 1 and 20?",
+  "What is americas pastime?",
+  "What color is the state of Kansas Flag?",
 ];
 
 var answers = [
-  "Steve",
-  "Yes",
-  "12",
+  "green",
+  "yes",
+  12,
+  "baseball",
+  "blue",
 ];
 
+var counter = 0;
+
 function game(questionAsked, answerInput, element) {
-  var guess = prompt(questionAsked);
-  if(guess === answerInput){
-      element.textContent = "Your answers is correct: " + guess;
-  } else if(guess === null) {
-      element.textContent = "Please answer the question ";
-  } else {
-      element.textContent = "Your answer is wrong: " + guess;
+  var guess = prompt(questionAsked).toLowerCase();
+  if (isNaN(parseInt(guess)) === false) {
+    guess = parseInt(guess);
   }
+    if(isNaN(guess)){
+      if(guess === answerInput){
+          element.textContent = "Your answers is correct: " + guess;
+          counter ++;
+      } else if(guess === null) {
+          element.textContent = "Please answer the question ";
+      } else {
+          element.textContent = "Your answer is wrong: " + guess;
+      }
+    } else {
+      if(guess === answers[i]) {
+        element.textContent = "You guessed the number correctly " + guess;
+        counter++
+      } else {
+        element.textContent = "Your number guess is wrong."
+      }
+
+    }
 }
+
 
 for (var i = 0; i< questions.length; i++){
   game(questions[i], answers[i], els[i]);
+
 }
+document.getElementById('totalRight').textContent = "You got "+ counter + " right";
